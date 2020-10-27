692dc1044b5725c5639263944eefc5bdc242f47a
PavelAll <alpa2020mi@yandex.ru>
2020-10-27 08:52:53 +0800

    1.1. Входные данные в тесте [#393779]

---
+++ src/ru/job4j/calculate/X2.java
@@ -0,0 +1,19 @@
+package ru.job4j.calculate;
+
+public class X2 {
+
+    public static int calc(int a, int b, int c, int x) {
+        int rsl = a * x * x + b * x + c;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        int c = 0;
+        int x = 1;
+
+        int rsl = X2.calc(a, b, c, x);
+        System.out.println(rsl);
+    }
+}
